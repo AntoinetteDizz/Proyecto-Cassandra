18c6d7e6dacb04f7637784e27a16d8b6cf873613
Kevinherrer1 <kevinherrerak14@gmail.com>
2024-01-03 17:15:29 -0400

    Actualización de rutas y lógica de panel principal

--- app.py
+++ app.py
@@ -1,44 +1,71 @@
-from flask import Flask, render_template, request, redirect, url_for 
-from cassandra.cluster import Cluster  
-  
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, url_for
+from cassandra.cluster import Cluster 
  
-# Conexión a Cassandra 
-cluster = Cluster(['127.0.0.1'])  # IP de  nodo de Cassandra 
-session = cluster.connect('supermarket')  # Reemplaza con tu keyspace 
+app = Flask(__name__) 
+
+# Conexión a Cassandra
+cluster = Cluster(['127.0.0.1'])  # IP de nodo de Cassandra
+session = cluster.connect('supermarket')  # Mi keyspace
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+#----------------------------------------rutas
+@app.route('/clientes')
+def clientes_index():
+    # Lógica para la página de clientes
+    return render_template('clientes/index.html')
+
+@app.route('/productos')
+def productos_index():
+    # Lógica para la página de productos
+    return render_template('productos/index.html')
+
+@app.route('/facturas')
+def facturas_index():
+    # Lógica para la página de facturas
+    return render_template('facturas/index.html')
+
+@app.route('/logout')
+def logout():
+    # Lógica para cerrar sesión
+    return redirect(url_for('index'))
+#-----------------------------------------rutas
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        email = request.form['username']
+        password = request.form['password']
+
+        # Realizar consulta a la base de datos Cassandra
+        query = "SELECT * FROM usuarios WHERE email = %s AND password = %s ALLOW FILTERING"
+        rows = session.execute(query, (email, password))
  
-@app.route('/') 
-def index(): 
-    return render_template('login.html') 
- 
-@app.route('/', methods=['GET', 'POST']) 
-def login(): 
-    if request.method == 'POST': 
-        # Obtener datos del formulario 
-        email = request.form['username'] 
-        password = request.form['password'] 
- 
-        # Realizar consulta a la base de datos Cassandra 
-        query = "SELECT * FROM usuarios WHERE email = %s AND password = %s ALLOW FILTERING" 
-        rows = session.execute(query, (email, password)) 
- 
-        # Verificar si los datos coinciden en la base de datos 
-        if rows: 
-            # Si coincide, redirigir a la página de panel 
-            return redirect(url_for('dashboard')) 
-        else: 
-            # Si no coincide, redirigir nuevamente a la página de login 
-            return redirect(url_for('login')) 
- 
-    # Si el método es GET o si la validación falla, mostrar la página de login 
-    return render_template('login') 
- 
-@app.route('/panel') 
-def dashboard(): 
-    # Lógica para la página del dashboard 
-    return render_template('panel.html') 
- 
- 
- 
-if __name__ == '__main__': 
+        # Verificar si los datos coinciden en la base de datos
+        if rows:
+            # Si coincide, redirigir a la página de panel
+            return redirect(url_for('panel'))
+        else:
+            # Si no coincide, redirigir nuevamente a la página de login
+            return redirect(url_for('login'))
+
+    # Si el método es GET o si la validación falla, mostrar la página de login
+    return render_template('login')
+
+##############################
+@app.route('/panel')
+def panel():
+     
+    # Realizar consulta a la base de datos Cassandra
+    rows = session.execute("SELECT * FROM clientes")
+
+    # Mostrar resultados en una plantilla HTML
+    return render_template('panel.html', rows=rows)
+##############################
+
+if __name__ == '__main__':
     app.run(debug=True)
